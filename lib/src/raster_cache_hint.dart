6dd7cb152bede8990ce50d99b5e04e5935bfb902
mjhd <mjhd.devlion@gmail.com>
2022-07-14 10:41:51 +0900

    add RasterCacheHint

---
+++ lib/src/raster_cache_hint.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+/// set `isComplex`, `willChange` hints to current DisplayList
+class RasterCacheHint extends SingleChildRenderObjectWidget {
+  const RasterCacheHint({
+    Key? key,
+    this.isComplex,
+    this.willChange,
+  }) : super(key: key);
+
+  final bool? isComplex;
+  final bool? willChange;
+
+  @override
+  void updateRenderObject(
+    BuildContext context,
+    _RasterCacheHintRenderObject renderObject,
+  ) {
+    renderObject.isComplex = isComplex ?? false;
+    renderObject.willChange = willChange ?? false;
+  }
+
+  @override
+  RenderObject createRenderObject(BuildContext context) =>
+      _RasterCacheHintRenderObject(isComplex, willChange);
+}
+
+class _RasterCacheHintRenderObject extends RenderProxyBox {
+  _RasterCacheHintRenderObject(bool? _isComplex, bool? _willChange)
+      : isComplex = _isComplex ?? false,
+        willChange = _willChange ?? false;
+
+  bool isComplex;
+  bool willChange;
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    if (isComplex) {
+      context.setIsComplexHint();
+    }
+
+    if (willChange) {
+      context.setWillChangeHint();
+    }
+
+    super.paint(context, offset);
+  }
+}
